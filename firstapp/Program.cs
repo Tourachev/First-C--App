b7d3fe797ac687d4ff3a37d9ec62d383ad11d6fa
Slav Tourachev <50384395+Tourachev@users.noreply.github.com>
2020-02-13 13:56:20 -0500

    First Commit

---
+++ firstapp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace firstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                var FibNumbers = new List<int> { 1, 1 };
+
+                while (FibNumbers.Count<20)
+                {
+                    var first = FibNumbers[FibNumbers.Count - 2];
+                    //Console.WriteLine($"First is {first}");
+
+                    var second = FibNumbers[FibNumbers.Count - 1];
+                    //Console.WriteLine($"Second is {second}");
+
+                    FibNumbers.Add(first + second);
+
+                }
+
+                foreach (var item in FibNumbers)
+                    Console.WriteLine(item);
+        }
+    }
+}
